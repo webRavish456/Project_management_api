22b84e762a86ba3ab38af1fe2f1e3a99be7cd183
Megha Raut <meghakumariraut064@gmail.com>
2025-04-09 14:10:44 +0530

    project

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,16 @@
 import express from 'express'
-import { postAdmin } from '../controllers/authControllers.js';
+import { postAdmin  } from '../controllers/authControllers.js';
+import { deleteProject, getProject, getProjectById, postProject, updateProject} from '../controllers/ProjectControllers.js';
 
 
 export const router = express.Router();
 
 router.route('/login').post(postAdmin);
+//router.route('/forgot').post(postForgot);
+
+
+router.route('/project').post(postProject)
+router.route('/project').get(getProject)
+router.route('/project/:id').get(getProjectById)
+router.route('/project/:id').patch(updateProject)
+router.route('/project/:id').delete(deleteProject)
