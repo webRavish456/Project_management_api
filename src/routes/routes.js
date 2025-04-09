f5f70e5f7e879cbd576c21cec69a624346fe2c59
Anjali Kumari <kumarianjali72794@gmail.com>
2025-04-09 13:28:50 +0530

    hello

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,15 @@
 import express from 'express'
-import { postAdmin } from '../controllers/authControllers.js';
+import { postAdmin, postForgot } from '../controllers/authControllers.js';
+import { deleteFinance, getFinance, getFinanceById, postFinance, updateFinance } from '../controllers/financeControllers.js';
 
 
 export const router = express.Router();
 
 router.route('/login').post(postAdmin);
+router.route('/forgot').post(postForgot);
+
+router.route('/finance').post(postFinance)
+router.route('/finance').get(getFinance)
+router.route('/finance/:id').get(getFinanceById)
+router.route('/finance/:id').patch(updateFinance)
+router.route('/finance/:id').delete(deleteFinance)
