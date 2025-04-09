0b0b99c2549096ce1f6261ca165cf96068c125cf
Neha-kumari-saw <nkumari7785@gmail.com>
2025-04-09 13:21:55 +0530

    client

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -50,3 +50,48 @@ export const postAdmin = async (req, res) => {
 
   } 
 };
+
+
+export const postForgot = async (req, res) => {
+
+  const ContentType = req.headers["content-type"];
+
+  if (ContentType && ContentType.includes("multipart/form-data")) {
+
+    upload.none()(req, res, async (err) => {
+      if (err) {
+        return res.status(500).json({ status: "error", msg: "Error handling form data" });
+      }
+
+      const { email, password, confirmpassword } = req.body;
+
+      if (!email || !password || !confirmpassword) {
+        return res.status(400).json({ status: "error", message: "Email and Password are required" });
+      }
+      if (password !== confirmpassword) {
+        return res.status(400).json({ status: "error", message: "Password do not match" });
+      }
+
+      try {
+        const existingAdmin = await AdminModel.findOne({ email });
+
+        if (!existingAdmin) {
+          return res.status(401).json({ status: "error", message: "Admin not found" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await AdminModel.updateOne(
+          { email },
+          { $set: { password: hashedPassword } }
+        );
+
+        res.status(200).json({ status: "success", message: "Password Reset Successfully!" });
+
+      } catch (error) {
+        console.log("Error during login:", error);
+        res.status(500).json({ status: "error", message: "Internal server error" });
+      }
+    });
+
+  }
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,15 @@
 import express from 'express'
-import { postAdmin } from '../controllers/authControllers.js';
+import { postAdmin, postForgot } from '../controllers/authControllers.js';
+import { postClient, getClient, getClientById, updateClient, deleteClient } from '../controllers/clientControllers.js';
 
 
 export const router = express.Router();
 
 router.route('/login').post(postAdmin);
+router.route('/forgot').post(postForgot);
+
+router.route('/client').post(postClient);
+router.route('/client').get(getClient);
+router.route('/client/:id').get(getClientById);
+router.route('/client/:id').patch(updateClient);
+router.route('/client/:id').delete(deleteClient);
