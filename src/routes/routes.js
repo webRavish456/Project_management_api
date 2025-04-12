29cb5a33ec7448d5098ae52bf1cd2a56e4f47c51
Anjali Kumari <kumarianjali72794@gmail.com>
2025-04-12 11:12:52 +0530

    finance backend commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,15 @@
 import express from 'express'
-//import { postAdmin, postForgot } from '../controllers/authControllers.js';
+import { postAdmin, postForgot } from '../controllers/authControllers.js';
 import { deleteTask, getTask, getTaskById, postTask, updateTask } from '../controllers/taskControllers.js';
 
 //import { postAdmin  } from '../controllers/authControllers.js';
 import { deleteProject, getProject, getProjectById, postProject, updateProject} from '../controllers/ProjectControllers.js';
 
-import { postAdmin, postForgot } from '../controllers/authControllers.js';
+//import { postAdmin, postForgot } from '../controllers/authControllers.js';
 import { deleteFinance, getFinance, getFinanceById, postFinance, updateFinance } from '../controllers/financeControllers.js';
 import { postClient, getClient, getClientById, updateClient, deleteClient } from '../controllers/clientControllers.js';
 import { postLeads, getLeads, updateLeads, deleteLeads, getLeadsById } from '../controllers/leadsControllers.js';
- import { get } from 'mongoose';
+import { get } from 'mongoose';
 import { deletedMeetingSchedule, getMeetingScheduleById, getMeetingSchedule, postMeetingSchedule, updatedMeetingSchedule} from '../controllers/meetingScheduledControllers.js';
 
 
@@ -31,13 +31,14 @@ router.route('/project').get(getProject)
 router.route('/project/:id').get(getProjectById)
 router.route('/project/:id').patch(updateProject)
 router.route('/project/:id').delete(deleteProject)
-router.route('/forgot').post(postForgot);
+//router.route('/forgot').post(postForgot);
 
 router.route('/finance').post(postFinance)
 router.route('/finance').get(getFinance)
 router.route('/finance/:id').get(getFinanceById)
 router.route('/finance/:id').patch(updateFinance)
 router.route('/finance/:id').delete(deleteFinance)
+
 router.route('/client').post(postClient);
 router.route('/client').get(getClient);
 router.route('/client/:id').get(getClientById);
